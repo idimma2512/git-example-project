af6b6a75ce7d8761d3abfe27935d25d78f1011ed
Idowu Emmanuel <idowu.immanuel17@gmail.com>
2023-11-01 11:26:19 +0100

    Simulating pull request

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Git
-        </a>
-      </header>
-    
-      <h1>Change from github editor</h1>
+      Simulating pull Request
     </div>
   );
 }
